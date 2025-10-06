dac89668a67913c1a267dc7bbafe35bad26588f6
J4nMm <Jeanm0571@gmail.com>
2025-10-06 15:50:29 -0300

    Update dados-atletas.js
    implementação dos métodos, sem a estrutura por enquanto

--- dados-atletas.js
+++ dados-atletas.js
@@ -6,4 +6,33 @@ class Atleta {
         this.altura = altura;
         this.notas = notas;
     }
+    //calcula a categoria do atleta
+    calculaCategoria(){}
+
+    //calcula o IMC do atleta
+    calculaIMC(){}
+
+    //calcula a média válida do atleta
+    calculaMediaValida(){}
+
+    //retorna o nome do atleta
+    obtemNomeAtleta(){}
+
+    //retorna a idade do atleta
+    obtemIdadeAtleta(){}
+
+    //retorna o peso do atleta
+    obtemPesoAtleta(){}
+
+    //retorna as notas do atleta
+    obtemNotasAtleta(){}
+
+    //retorna a categoria do atleta
+    obtemCategoria(){}
+
+    //retorna o IMC do atleta
+    obtemIMC(){}
+
+    //retorna a média válida do atleta
+    obtemMediaValida(){}
 }
