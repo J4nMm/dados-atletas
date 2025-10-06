0271b85f244ddce004c6cf9926e2927726694ef7
J4nMm <Jeanm0571@gmail.com>
2025-10-06 20:35:17 -0300

    Implementa métodos da classe Atleta
    Adiciona implementações para os métodos de cálculo de categoria, IMC, média válida e métodos de obtenção de dados do atleta.

--- dados-atletas.js
+++ dados-atletas.js
@@ -7,32 +7,83 @@ class Atleta {
         this.notas = notas;
     }
     //calcula a categoria do atleta
-    calculaCategoria(){}
+    calculaCategoria(){
+        if(this.idade >= 9 && this.idade <= 11){
+            return "infantil"
+        }
+        else if(this.idade >= 12 && this.idade <= 13){
+            return "juvenil"
+        }
+        else if(this.idade >= 14 && this.idade <= 15){
+            return "intermediário"
+        }
+        else if(this.idade >= 16 && this.idade <= 30){
+            return "adulto"
+        }
+        else{
+            return "Sem categoria"
+        }
+    }
 
     //calcula o IMC do atleta
-    calculaIMC(){}
+    calculaIMC(){
+        return this.peso / (this.altura * this.altura);
+    }
 
     //calcula a média válida do atleta
-    calculaMediaValida(){}
+    calculaMediaValida(){
+        let notas = [...this.notas];
+
+        notas.sort((a, b) => a - b);
+
+      notas = notas.slice(1, 4);
+
+      let soma = notas.reduce((acum, atual) => acum + atual, 0);
+
+      let media = soma / notas.length;
+      return media;
+    }
 
     //retorna o nome do atleta
-    obtemNomeAtleta(){}
+    obtemNomeAtleta(){
+        console.log(`Nome: ${this.nome}`);
+    }
 
     //retorna a idade do atleta
-    obtemIdadeAtleta(){}
+    obtemIdadeAtleta(){
+        console.log(`Idade: ${this.idade}`);
+    }
 
     //retorna o peso do atleta
-    obtemPesoAtleta(){}
+    obtemPesoAtleta(){
+        console.log(`Peso: ${this.peso}`);
+    }
 
     //retorna as notas do atleta
-    obtemNotasAtleta(){}
+    obtemNotasAtleta(){
+        console.log(`Notas: ${this.notas}`);
+    }
 
     //retorna a categoria do atleta
-    obtemCategoria(){}
+    obtemCategoria(){
+        console.log(`Categoria: ${this.calculaCategoria()}`);
+    }
 
     //retorna o IMC do atleta
-    obtemIMC(){}
+    obtemIMC(){
+        console.log(`IMC: ${this.calculaIMC()}`)
+    }
 
     //retorna a média válida do atleta
-    obtemMediaValida(){}
+    obtemMediaValida(){
+        console.log(`Média Válida: ${this.calculaMediaValida()}`);
+    }
 }
+let atleta = new Atleta("João Pedro", 18, 70, 1.75, [9, 10, 6.7, 4, 8.5]);
+atleta.obtemNomeAtleta();
+atleta.obtemIdadeAtleta();
+atleta.obtemPesoAtleta();
+atleta.obtemNotasAtleta();
+atleta.obtemCategoria();
+atleta.obtemIMC();
+atleta.obtemMediaValida();
